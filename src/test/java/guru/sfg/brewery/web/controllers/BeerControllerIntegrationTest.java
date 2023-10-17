78f1bfd11ee89baadcceb5ec3b12e9dc47d0050f
LyraFromCodlan <nurtaytulegenov@gmail.com>
2023-10-18 01:42:29 +0600

    Added another method testing HTTP endpoint with Basic Authorization with static method and values imported from .property file

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIntegrationTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import guru.sfg.brewery.services.BreweryService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -16,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -41,6 +43,12 @@ public class BeerControllerIntegrationTest {
     @MockBean
     BreweryService breweryService;
 
+    @Value("${spring.security.user.name}")
+    String name;
+
+    @Value("${spring.security.user.password}")
+    String password;
+
 
     @BeforeEach
     void setUpMockMvcFromAppContext(){
@@ -56,6 +64,13 @@ public class BeerControllerIntegrationTest {
     @WithMockUser("Lyra Hearthstrings")
     void findBeers() throws Exception{
         mockMvc.perform(get("/beers/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+    @Test
+    void findBeersWithHttpBasic() throws Exception{
+        mockMvc.perform(get("/beers/find").with(httpBasic(name,password)))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
